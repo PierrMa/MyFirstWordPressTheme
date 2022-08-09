b13c7e658c767465819ae8c196c79a8b7c8bff84
Pierre <pierremariefrederic@gmail.com>
2022-08-09 16:51:07 +0200

    making the post template design responsive

--- template-parts/content.php
+++ template-parts/content.php
@@ -29,15 +29,15 @@
 		if ( 'post' === get_post_type() ) :
 			?>
 			<div class="entry-meta pmf-entry-meta">				
-				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+				<p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
   					<path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
 				</svg>
-				<?php testtheme_posted_by(); ?>		
-				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
+				<?php testtheme_posted_by(); ?></p>		
+				<p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
   					<path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
 				</svg>
-				<?php testtheme_posted_on(); ?>
-				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16">
+				<?php testtheme_posted_on(); ?></p>
+				<p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16">
   					<path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
 				</svg>
 				<?php 
@@ -46,15 +46,14 @@
 						echo $amountOfComment;
 					else:?>
 					<span>No Comments</span>
-				<?php endif; ?>
+				<?php endif; ?></p>
 			</div--><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
-
+	<p id="wordCount"></p>
 	<?php testtheme_post_thumbnail(); ?>
 	
 	<div class="entry-content post-entry-content">
-		
 		<?php
 		the_content(
 			sprintf(
@@ -77,8 +76,6 @@
 			)
 		);
 		?>
-		<!-- <input type ="hidden" id="phpPostContent" value='<?php the_content()?>'/> -->
-		<span id="wordCount"></span>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer post-entry-footer">
